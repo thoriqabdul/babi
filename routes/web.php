2b9ea36c1a16681360f8016e995d8978bbbe6ef4
thoriqabdul <thoriqabdul.ta@gmail.com>
2020-08-02 00:32:03 +0700

    aji belajar

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+route::group(['prefix' => 'admin','middleware'=>['auth','itsAdmin']], function(){
+    Route::get('/', 'AdminController@index')->name('admin.index');
+    // Route::get('/home', 'HomeController@index')->name('home');
+});
+
+
+
+
+route::group(['middleware'=>['auth']], function(){
+    Route::get('/', 'ClientController@index')->name('client.index');
+});
