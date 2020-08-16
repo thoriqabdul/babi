f11f04cb4f38d38a7883f9d4c720fd9a47fbc139
thoriqabdul <thoriqabdul.ta@gmail.com>
2020-08-16 14:17:44 +0700

    a

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,6 +19,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $model = App\User::get();
+        return view('home', compact('model'));
     }
 }
